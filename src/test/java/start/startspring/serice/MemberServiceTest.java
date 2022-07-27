020e41ecf179f06cdf39c2f2e880157d9e2bb606
JeongJoonMo <85468465+JeongJoonMo@users.noreply.github.com>
2022-07-27 17:42:37 +0900

    day 1

---
+++ src/test/java/start/startspring/serice/MemberServiceTest.java
@@ -0,0 +1,51 @@
+package start.startspring.serice;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import start.startspring.domain.Member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복회원예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("sping");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+
+        //then
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
